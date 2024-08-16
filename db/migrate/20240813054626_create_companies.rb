c33077f8035ba7965a7b7e4f7f4d793948715fc8
Bruno Pagno <brunopagno@proton.me>
2024-08-16 08:52:40 +0200

    create base company model and association to user

---
+++ db/migrate/20240813054626_create_companies.rb
@@ -0,0 +1,10 @@
+class CreateCompanies < ActiveRecord::Migration[7.1]
+  def change
+    create_table :companies do |t|
+      t.string :name, null: false, default: ""
+      t.references :owner, foreign_key: { to_table: :users }
+
+      t.timestamps
+    end
+  end
+end
