a93dc97d4c02924ca8cc60a73155151996d0dc21
Bruno Pagno <brunopagno@proton.me>
2024-08-17 18:32:11 +0200

    update shares to not allow null FKs

--- db/migrate/20240816084115_create_shares.rb
+++ db/migrate/20240816084115_create_shares.rb
@@ -2,8 +2,8 @@ class CreateShares < ActiveRecord::Migration[7.1]
   def change
     create_table :shares do |t|
       t.boolean :active, null: false, default: true
-      t.references :parent, foreign_key: { to_table: :companies }
-      t.references :child, foreign_key: { to_table: :companies }
+      t.references :parent, foreign_key: { to_table: :companies }, null: false
+      t.references :child, foreign_key: { to_table: :companies }, null: false
 
       t.timestamps
     end
